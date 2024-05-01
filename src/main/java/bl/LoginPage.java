af08e85d36176b5c55836960cdaf3ec057cde572
Michael Bramnik <88651665+michaelbramnik@users.noreply.github.com>
2024-05-02 01:15:30 +0300

    Initial commit

---
+++ src/main/java/bl/LoginPage.java
@@ -0,0 +1,43 @@
+package bl;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.assertions.LocatorAssertions;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class LoginPage {
+    private Page page = null;
+    private Locator loginForm = null;
+
+    public LoginPage(Page page) {
+        this.page = page;
+    }
+
+    public void openLoginForm() {
+        Locator signInButton = page.locator(".HeaderMenu-link--sign-in");
+        assertThat(signInButton).isVisible();
+        signInButton.click();
+
+        loginForm = page.locator(".auth-form-body");
+        assertThat(loginForm).isVisible();
+    };
+
+    public void signIn(String username, String password, Boolean expectFail) {
+        this.openLoginForm();
+        this.loginForm.locator("#login_field").fill(username);
+        this.loginForm.locator("#password").fill(password);
+        this.loginForm.locator(".btn-primary").click();
+
+        // Handle Device verification form
+        if (expectFail) {
+            assertThat(this.page.locator(".js-flash-alert")).hasText(
+                    "Incorrect username or password.",
+                    new LocatorAssertions.HasTextOptions().setUseInnerText(true));
+        } else {
+            assertThat(this.page.locator(".AppHeader")).isVisible();
+            // Check the name of the logged-in user matched the provided credentials
+        }
+
+    }
+}
---
+++ src/main/java/tests/TestGitHubSanity.java
@@ -0,0 +1,60 @@
+package tests;
+
+import bl.LoginPage;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.LoadState;
+import org.testng.annotations.*;
+
+
+public class TestGitHubSanity {
+    Playwright playwright;
+    Browser browser;
+
+    // New instance for each test method.
+    BrowserContext context;
+    Page page;
+
+    @BeforeClass
+    void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+    }
+
+    @AfterClass
+    void closeBrowser() {
+        playwright.close();
+    }
+
+    @BeforeMethod
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+        page.navigate("https://github.com/");
+        page.waitForLoadState(LoadState.DOMCONTENTLOADED);
+    }
+
+    @AfterMethod
+    void closeContext() {
+        context.close();
+    }
+
+    @Test
+    void loginToGithub() {
+        LoginPage loginPage = new LoginPage(page);
+        // Pass on valid credentials
+        loginPage.signIn("aaa", "bbb", false);
+    }
+
+    @Test
+    void loginToGithubNegative() {
+        LoginPage loginPage = new LoginPage(page);
+        loginPage.signIn("111", "222", true);
+    }
+
+    @Test
+    void getUserRepos() {
+        LoginPage loginPage = new LoginPage(page);
+        // Pass on valid credentials
+        loginPage.signIn("aaa", "bbb", false);
+    }
+}
